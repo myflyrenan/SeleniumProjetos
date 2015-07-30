de20b2e5ed642b49182e7061c662c5190174eff7
renan.moura <renan.moura@desa75.techne.com.br>
2015-07-30 10:30:45 -0300

    Silenium inicialização

---
+++ Ergon V1.1/src/ergon/v1/pkg1/ErgonV11.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ergon.v1.pkg1;
+
+/**
+ *
+ * @author renan.moura
+ */
+public class ErgonV11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
